a8adeb0721321eef71266b1eca27cac79efff5fa
mustafaKamal-fe <54776055+mustafaKamal-fe@users.noreply.github.com>
2020-06-12 20:00:41 +0300

    initial test

---
+++ build/precache-manifest.9032b1a23988a7c95c114a90866f384a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d91087d21cb4b140e5661fe6624383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481003502097901c5212",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5cc666bdb7a537396ba9",
+    "url": "/static/js/2.b7891638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7891638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481003502097901c5212",
+    "url": "/static/js/main.b08568a1.chunk.js"
+  },
+  {
+    "revision": "85cfd3accad61e99c8bc",
+    "url": "/static/js/runtime-main.e65b7373.js"
+  },
+  {
+    "revision": "15b75f5a1167ff4983077d570f7c6574",
+    "url": "/static/media/video.15b75f5a.mp4"
+  }
+]);
